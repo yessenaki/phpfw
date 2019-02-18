9359f0e323a33cc445a7e2029448cf874657aa0b
Yessen Orazbaiuly <yessenunique@gmail.com>
2019-02-18 11:47:07 +0600

    Get the controller and action from a URL with a fixed structure

--- Core/Router.php
+++ Core/Router.php
@@ -17,11 +17,28 @@ class Router
 
     public function match($url)
     {
-        foreach ($this->routes as $route => $params) {
+        /* foreach ($this->routes as $route => $params) {
             if ($url == $route) {
                 $this->params = $params;
                 return true;
             }
+        } */
+
+        // Match to the fixed URL format /controller/action
+        $reg_exp = "/^(?P<controller>[a-z-]+)\/(?P<action>[a-z-]+)$/";
+
+        if (preg_match($reg_exp, $url, $matches)) {
+            // Get named capture group values
+            $params = [];
+
+            foreach ($matches as $key => $match) {
+                if (is_string($key)) {
+                    $params[$key] = $match;
+                }
+            }
+
+            $this->params = $params;
+            return true;
         }
 
         return false;
