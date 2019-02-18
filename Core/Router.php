9ea3cd2be506433089fc5c06f1aec03774f55181
Оразбайұлы Есен <y.orazbaiuly@amanat.local>
2019-02-18 10:25:38 +0600

    Match the requested route to the list of routes in the routing table

---
+++ Core/Router.php
@@ -0,0 +1,34 @@
+<?php
+
+class Router
+{
+    protected $routes = [];
+    protected $params = [];
+
+    public function add($route, $params)
+    {
+        $this->routes[$route] = $params; 
+    }
+
+    public function getRoutes()
+    {
+        return $this->routes;
+    }
+
+    public function match($url)
+    {
+        foreach ($this->routes as $route => $params) {
+            if ($url == $route) {
+                $this->params = $params;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public function getParams()
+    {
+        return $this->params;
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,3 +1,28 @@
 <?php
 
-echo 'Requested URL = "' . $_SERVER['QUERY_STRING'] . '"';
+// echo 'Requested URL = "' . $_SERVER['QUERY_STRING'] . '"';
+require '../Core/Router.php';
+
+$router = new Router();
+// echo get_class($router);
+
+// Add the routes
+$router->add('', ['controller' => 'Home', 'action' => 'index']);
+$router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
+$router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
+
+// Display the routing table
+// echo '<pre>';
+// print_r($router->getRoutes());
+// echo '</pre>';
+
+// Match the requested route
+$url = $_SERVER['QUERY_STRING'];
+
+if ($router->match($url)) {
+    echo '<pre>';
+    var_dump($router->getParams());
+    echo '</pre>';
+} else {
+    echo "No route found for URL '$url'";
+}
