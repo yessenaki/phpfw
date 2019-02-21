563b774f1c7bd0e5572ca57988ecccb6458701df
Yessen Orazbaiuly <yessenunique@gmail.com>
2019-02-21 16:20:34 +0600

    Output escaping what it is, why do it, and how and when to do it

--- App/Views/Home/index.php
+++ App/Views/Home/index.php
@@ -4,10 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home</title>
+    <title>Output escaping</title>
 </head>
 <body>
-    <h1>Welcome</h1>
-    <p>Hello from the view!</p>
+    <h1>Output escaping</h1>
+    <?php
+        // Display the results of submitting the form
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            echo "Hello, " . htmlspecialchars($_POST['name']);
+        }
+    ?>
+    <form action="" method="post">
+        <div>
+            <label for="name">Your name</label>
+            <input type="text" name="name" id="name" autofocus>
+        </div>
+        <div>
+            <button type="submit">Submit</button>
+        </div>
+    </form>
 </body>
 </html>
