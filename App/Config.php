9b5f695a1ea5429820e81185e265cff173841413
Yessen Orazbaiuly <yessenunique@gmail.com>
2019-02-22 15:18:36 +0600

    Put app config settings in a separate file

---
+++ App/Config.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+class Config
+{
+    const DB_HOST = 'localhost';
+    const DB_NAME = 'phpfw';
+    const DB_USER = 'root';
+    const DB_PASSWORD = '';
+}
--- App/Models/Post.php
+++ App/Models/Post.php
@@ -2,19 +2,15 @@
 
 namespace App\Models;
 
+use Core\Model;
 use PDO;
 
-class Post
+class Post extends Model
 {
     public static function getAll()
     {
-        $host = 'localhost';
-        $dbname = 'phpfw';
-        $username = 'root';
-        $password = '';
-
         try {
-            $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8, $username, $password");
+            $db = static::getDB();
             $stmt = $db->query('SELECT id, title, content FROM posts ORDER BY created_at');
             $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
             return $results;
